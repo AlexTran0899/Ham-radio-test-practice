b0f25ebcfcf112e2438a6ee7df04c917d5620df1
AlexTran0899 <76791231+AlexTran0899@users.noreply.github.com>
2021-10-27 19:57:50 -0500

    4/5 of the way there

--- api/data/migrations/02-questions.js
+++ api/data/migrations/02-questions.js
@@ -1,18 +1,19 @@
 exports.up = async (knex) => {
     await knex.schema
-      .createTable('questions', (users) => {
-        users.integer('class')
-        users.integer('subelement')
-        users.integer('section')
-        users.integer('question_number')
-        users.string('question')
-        users.string('correct_answer')
-        users.string('A')
-        users.string('B')
-        users.string('C')
-        users.string('D')
-        users.string('explanation')
-        users.string('video_url')
+      .createTable('questions', (questions) => {
+        questions.increments('question_id')
+        questions.integer('class')
+        questions.integer('subelement')
+        questions.integer('section')
+        questions.integer('question_number')
+        questions.string('question')
+        questions.string('correct_answer')
+        questions.string('A')
+        questions.string('B')
+        questions.string('C')
+        questions.string('D')
+        questions.string('explanation')
+        questions.string('video_url')
       })
   }
   
