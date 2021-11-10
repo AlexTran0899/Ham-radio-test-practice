f1a34d10add53b3c3819721c141ba936f813693c
AlexTran0899 <76791231+AlexTran0899@users.noreply.github.com>
2021-11-10 11:51:48 -0600

    add more stlying and remove some description on homepage

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,73 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+const { test, expect } = require('@playwright/test');
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('test', async ({ page }) => {
+
+  // Go to http://localhost:3000/
+  await page.goto('http://localhost:3000/');
+
+  // Click text=Technician
+  await page.click('text=Technician');
+  await expect(page).toHaveURL('http://localhost:3000/Technician');
+
+  // Click text=Electrical safety
+  await page.click('text=Electrical safety');
+  await expect(page).toHaveURL('http://localhost:3000/questions');
+
+  // Click text=A. Touching both terminals with the hands can cause electrical shock
+  await page.click('text=A. Touching both terminals with the hands can cause electrical shock');
+
+  // Click text=B. Shorting the terminals can cause burns, fire, or an explosion
+  await page.click('text=B. Shorting the terminals can cause burns, fire, or an explosion');
+
+  // Click text=C. RF emissions from the battery
+  await page.click('text=C. RF emissions from the battery');
+
+  // Click text=D. All of these choices are correct
+  await page.click('text=D. All of these choices are correct');
+
+  // Click text=next
+  await page.click('text=next');
+
+  // Click text=C. It may cause involuntary muscle contractions
+  await page.click('text=C. It may cause involuntary muscle contractions');
+
+  // Click text=B. It may disrupt the electrical functions of cells
+  await page.click('text=B. It may disrupt the electrical functions of cells');
+
+  // Click text=A. It may cause injury by heating tissue
+  await page.click('text=A. It may cause injury by heating tissue');
+
+  // Click text=D. All of these choices are correct
+  await page.click('text=D. All of these choices are correct');
+
+  // Click text=next
+  await page.click('text=next');
+
+  // Click text=D. The white wire
+  await page.click('text=D. The white wire');
+
+  // Click text=B. Hot
+  await page.click('text=B. Hot');
+
+  // Click text=A. Neutral
+  await page.click('text=A. Neutral');
+
+  // Click text=C. Equipment ground
+  await page.click('text=C. Equipment ground');
+
+  // Click text=next
+  await page.click('text=next');
+
+  // Click text=C. To limit current to prevent shocks
+  await page.click('text=C. To limit current to prevent shocks');
+
+  // Click text=A. To prevent power supply ripple from damaging a circuit
+  await page.click('text=A. To prevent power supply ripple from damaging a circuit');
+
+  // Click text=B. To interrupt power in case of overload
+  await page.click('text=B. To interrupt power in case of overload');
+
+  // Click text=next
+  await page.click('text=next');
+
 });
--- client/src/Components/class.js
+++ client/src/Components/class.js
@@ -21,32 +21,12 @@ const Class = (props) => {
             <div className='classes'>
                 {props.class === 'Technician' || props.class === undefined ? <div className='classButton' onClick={() => push('/Technician')}>
                     <h1>Technician</h1>
-                    <ul>
-                        <li>- 423 questions</li>
-                        <li>- Two weeks to study</li>
-                        <br />
-                        <li>- 1500 watt max transmit power</li>
-                        <li>- UHF 23, 33, 70 Centimeters</li>
-                        <li>- VHF 1.25, 2, 6 Meters</li>
-                        <li>- HF 10</li>
-                        <li>- HF 15, 40, 80 Meters CW ONLY</li>
-                    </ul>
                 </div> : null}
                 {props.class === 'General' || props.class === undefined ? <div className='classButton' onClick={() => push('/General')}>
                     <h1>General</h1>
-                    <ul>
-                        <li>- 423 questions</li>
-                        <li>- Two weeks to study</li>
-                        <li>- item one</li>
-                    </ul>
                 </div> : null}
                 {props.class === 'AmateurExtra' || props.class === undefined ? <div className='classButton' onClick={() => push('/AmateurExtra')}>
                     <h1>Amateur Extra</h1>
-                    <ul>
-                        <li>- 423 questions</li>
-                        <li>- Two weeks to study</li>
-                        <li>- item one</li>
-                    </ul>
                 </div> : null}
             </div>
         </div>
